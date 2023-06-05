ad5c9c62b6756362b751af2512fbfda4507ba1ab
Wiggidy <rlr0017@auburn.edu>
2023-06-05 16:28:36 -0500

    attempt 2 at accepting Vec<String> as characters

--- src/validate.rs
+++ src/validate.rs
@@ -10,15 +10,14 @@ pub struct Response {
 }
 
 pub async fn validate<
-    's,
-    S: AsRef<&'s str>,
+    S: AsRef<str>,
     C: hyper::client::connect::Connect + Clone + Send + Sync + 'static,
 >(
     hyper_client: &hyper::client::Client<C, hyper::body::Body>,
     jwks: &JWKS,
     refresh_token: &str,
     client_id: &str,
-    mut characters: impl Iterator<Item = S> + 's,
+    mut characters: impl Iterator<Item = S>,
 ) -> Result<Response, Error> {
     let oauth_rep = AuthenticationResponse::new(
         hyper_client,
